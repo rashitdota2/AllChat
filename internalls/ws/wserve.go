f813b6bdddda66626ecc58d289167b16a92ba290
rashitdota2 <rashitdota2@gmail.com>
2024-06-25 16:23:37 +0500

    debug socket

--- internalls/ws/wserve.go
+++ internalls/ws/wserve.go
@@ -9,12 +9,14 @@ var Clients = make(map[*websocket.Conn]bool)
 var Broadcast = make(chan models.Message)
 
 func WSRun() {
-	msg := <-Broadcast
-	for client := range Clients {
-		err := client.WriteJSON(msg)
-		if err != nil {
-			client.Close()
-			delete(Clients, client)
+	for {
+		msg := <-Broadcast
+		for client := range Clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				client.Close()
+				delete(Clients, client)
+			}
 		}
 	}
 }
